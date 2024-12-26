858b5e673b2ff666da5e477b758200fe076f02a9
ADMIN\Admin <cuongkoi1406@gmail.com>
2024-12-26 17:36:53 +0700

    first commit

---
+++ app/src/main/java/com/example/appholaagri/ForgotPasswordActivity.java
@@ -0,0 +1,33 @@
+package com.example.appholaagri;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+    Button btnBackLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_forgot_password);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        btnBackLogin = findViewById(R.id.back_btn);
+        btnBackLogin.setOnClickListener(view -> {
+            onBackPressed();
+        });
+
+    }
+    public void onBackPressed() {
+        super.onBackPressed(); // Quay lại trang trước đó
+    }
+}
